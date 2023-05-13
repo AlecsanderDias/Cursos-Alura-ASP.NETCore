b9190159ed8124cd2c6e6a2d9653a06bc6b08a6d
Alecsander <alecsanderdias@hotmail.com>
2023-05-13 12:13:14 -0300

    Iniciando módulo 4 do Ccurso de .NET 6: criando uma web API

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Filme
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O título do filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage ="O título do filme deve ter no máximo até 50 caracteres!")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O gênero do filme é obrigatório!")]
+    [MaxLength(50, ErrorMessage = "O gênerodo filme deve ter no máximo até 20 caracteres!")]
+    public string Genero { get; set; }
+
+    [Range(70, 600, ErrorMessage = "A duração do filme deve ser entre 70 e 600 minutos!")]
+    public int Duracao { get; set; }
+
+    public Filme(string Titulo, string Genero, int Duracao)
+    {
+        this.Titulo = Titulo;
+        this.Genero = Genero;
+        this.Duracao = Duracao;
+    }
+
+    public override string ToString()
+    {
+        return  $"Título do Filme:  {Titulo} \n" +
+                $"Gênero do Filme:  {Genero} \n" +
+                $"Duração do Filme: {Duracao}";
+    }
+}
