ac5da97390a0fe576b972e1da59c788e6356d053
Alecsander <alecsanderdias@hotmail.com>
2023-05-23 23:06:29 -0300

    Iniciando Curso de .NET 6 e Identity: implementando controle de usuário

---
+++ UsuariosAPI/Profiles/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<CreateUsusarioDto, Usuario>();
+    }
+}
